c507f0c57d6a538185e3205b327b5582340d318a
Ciaran Concannon <cjcon90@pm.me>
2021-05-22 01:02:21 +0100

    added mobile nav menu

--- rhythmbox/store/static/store/scripts/app.js
+++ rhythmbox/store/static/store/scripts/app.js
@@ -1,5 +1,11 @@
 const hamburger = document.querySelector('.nav__hamburger')
+const menu = document.querySelector('.nav-menu')
+const menuList = menu.children
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('open')
+    hamburger.classList.toggle('open');
+    menu.classList.toggle('hidden');
+    for(let i = 0; i < menuList.length; i++) {
+        menuList[i].classList.toggle('nav-list-appear')
+    }
 })
