702ec0ac1dc07173021800fef5b55412e093d835
Ciaran Concannon <cjcon90@pm.me>
2021-05-25 18:16:05 +0100

    add homepage styling for tablet viewports

--- rhythmbox/store/static/store/scripts/app.js
+++ rhythmbox/store/static/store/scripts/app.js
@@ -1,7 +1,8 @@
 const hamburger = document.querySelector('.nav__hamburger')
 const menu = document.querySelector('.nav-menu')
-const menuList = menu.children
+const menuList = document.querySelectorAll('li.nav-menu__list--category, li.nav-menu__list--subcategory')
 
+// Add animation to nav menu items in mobile view
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('open');
     menu.classList.toggle('hidden');
