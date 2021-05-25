ad7ec5b9ab5852b1741ada86ecc76a8fa09d9015
Ciaran Concannon <cjcon90@pm.me>
2021-05-25 21:26:15 +0100

    add product card styling

--- rhythmbox/store/views.py
+++ rhythmbox/store/views.py
@@ -1,4 +1,4 @@
-from products.models import Product
+# from products.models import Product
 from django.shortcuts import render
 
 def index(request):
@@ -9,8 +9,64 @@ def index(request):
 
 
 def shop(request):
-    products = Product.objects.all()
+    # products = Product.objects.all()
     context = {
-        'products': products
+        'products': [
+    {
+        "pk": 1,
+        "title": "Pearl DC1450S Dennis Chambers Snare",
+        "slug": "pearl-dc1450s-dennis-chambers-snare",
+        "description": "Considered by many to be one of the greatest drummers of all time, Pearl's Signature Snare drum collaboration with the great Dennis Chambers is a celebration of three decades of partnership, and a unique tribute to his diverse playing talents.",
+        "price": "877.00",
+        "rating": (4.47 / 5) * 100,
+        "num_ratings": 15,
+        "image": "https://thumbs.static-thomann.de/thumb/orig/pics/bdb/382510/11288892_800.webp",
+        "brand_image": "https://images.static-thomann.de/pics/herstlogos/pearl.gif",
+    },
+    {
+        "pk": 2,
+        "title": "Pearl DC1450S Dennis Chambers Snare",
+        "slug": "pearl-dc1450s-dennis-chambers-snare",
+        "description": "Considered by many to be one of the greatest drummers of all time, Pearl's Signature Snare drum collaboration with the great Dennis Chambers is a celebration of three decades of partnership, and a unique tribute to his diverse playing talents.",
+        "price": "877.00",
+        "rating": (4.47 / 5) * 100,
+        "num_ratings": 15,
+        "image": "https://thumbs.static-thomann.de/thumb/orig/pics/bdb/382510/11288892_800.webp",
+        "brand_image": "https://images.static-thomann.de/pics/herstlogos/pearl.gif",
+    },
+    {
+        "pk": 3,
+        "title": "Pearl DC1450S Dennis Chambers Snare",
+        "slug": "pearl-dc1450s-dennis-chambers-snare",
+        "description": "Considered by many to be one of the greatest drummers of all time, Pearl's Signature Snare drum collaboration with the great Dennis Chambers is a celebration of three decades of partnership, and a unique tribute to his diverse playing talents.",
+        "price": "877.00",
+        "rating": (4.47 / 5) * 100,
+        "num_ratings": 15,
+        "image": "https://thumbs.static-thomann.de/thumb/orig/pics/bdb/382510/11288892_800.webp",
+        "brand_image": "https://images.static-thomann.de/pics/herstlogos/pearl.gif",
+    },
+    {
+        "pk": 4,
+        "title": "Pearl DC1450S Dennis Chambers Snare",
+        "slug": "pearl-dc1450s-dennis-chambers-snare",
+        "description": "Considered by many to be one of the greatest drummers of all time, Pearl's Signature Snare drum collaboration with the great Dennis Chambers is a celebration of three decades of partnership, and a unique tribute to his diverse playing talents.",
+        "price": "877.00",
+        "rating": (4.47 / 5) * 100,
+        "num_ratings": 15,
+        "image": "https://thumbs.static-thomann.de/thumb/orig/pics/bdb/382510/11288892_800.webp",
+        "brand_image": "https://images.static-thomann.de/pics/herstlogos/pearl.gif",
+    },
+    {
+        "pk": 5,
+        "title": "Pearl DC1450S Dennis Chambers Snare",
+        "slug": "pearl-dc1450s-dennis-chambers-snare",
+        "description": "Considered by many to be one of the greatest drummers of all time, Pearl's Signature Snare drum collaboration with the great Dennis Chambers is a celebration of three decades of partnership, and a unique tribute to his diverse playing talents.",
+        "price": "877.00",
+        "rating": (4.47 / 5) * 100,
+        "num_ratings": 15,
+        "image": "https://thumbs.static-thomann.de/thumb/orig/pics/bdb/382510/11288892_800.webp",
+        "brand_image": "https://images.static-thomann.de/pics/herstlogos/pearl.gif",
+    },
+]
     }
     return render(request, 'store/shop.html' , context)
