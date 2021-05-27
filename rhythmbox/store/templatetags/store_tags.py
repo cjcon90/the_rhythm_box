7fe9523dcee1a03ec6092a45cdd28af725491b29
Ciaran Concannon <cjcon90@pm.me>
2021-05-27 23:34:32 +0100

    add pagination on shop page

--- rhythmbox/store/templatetags/store_tags.py
+++ rhythmbox/store/templatetags/store_tags.py
@@ -6,5 +6,7 @@ register = template.Library()
 @register.simple_tag
 def url_replace(request, field, value):
     dict_ = request.GET.copy()
+    print('=================================')
+    print(field)
     dict_[field] = value
     return dict_.urlencode()
--- rhythmbox/store/views.py
+++ rhythmbox/store/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from django.shortcuts import render
 from urllib.parse import urlparse, urlunparse
 from django.http import QueryDict
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -25,16 +26,13 @@ def shop(request, category=None, subcategory=None, type=None):
         q &= Q(category__slug=category)
 
     order = request.GET.get('order_by') or '-date_added' # '?order_by=*param'
-    
-    context['products'] = Product.objects.filter(q).order_by(order)
+    products = Product.objects.filter(q).order_by(order)
+    paginator = Paginator(products, 3)
+    page_num = request.GET.get('page')
+    context['page_obj'] = paginator.get_page(page_num)
+
     context['categories'] = Category.objects.all()
+    context['products'] = products
+
     return render(request, 'store/shop.html' , context)
-
-
-# def replace_query_param(url, attr, val):
-#     (scheme, netloc, path, params, query, fragment) = urlparse(url)
-#     query_dict = QueryDict(query).copy()
-#     query_dict[attr] = val
-#     query = query_dict.urlencode()
-#     return urlunparse((scheme, netloc, path, params, query, fragment))
 
