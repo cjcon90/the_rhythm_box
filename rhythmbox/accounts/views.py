474907c7cbc03b43bc1c6da4fa03c5d29d63a58c
Ciaran Concannon <cjcon90@pm.me>
2021-05-30 12:23:53 +0100

    Fix error messages not showing on login form

--- rhythmbox/accounts/views.py
+++ rhythmbox/accounts/views.py
@@ -1,52 +1,60 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from .forms import RegistrationForm, AccountAuthenticationForm
+from django.contrib import messages
+from django.utils.safestring import mark_safe
 
 
 def registration_view(request):
-	context = {}
-	if request.POST:
-		form = RegistrationForm(request.POST)
-		if form.is_valid():
-			form.save()
-			email = form.cleaned_data.get('email')
-			raw_password = form.cleaned_data.get('password1')
-			account = authenticate(email=email, password=raw_password)
-			login(request, account)
-			return redirect('home')
-		else:
-			context['registration_form'] = form
-	else: #GET request
-		form = RegistrationForm()
-		context['registration_form'] = form
-	return render(request, 'accounts/register.html', context)
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            messages.success(request, mark_safe(
+                f'Thanks for registering, {request.user.first_name}!<br/>'
+                f'You are now logged in 🙂'))
+            return redirect('home')
+        else:
+            context['registration_form'] = form
+    else:  # GET request
+        form = RegistrationForm()
+        context['registration_form'] = form
+    return render(request, 'accounts/register.html', context)
+
 
 def logout_view(request):
-	logout(request)
-	return redirect('/')
+    logout(request)
+    return redirect('/')
 
 
 def login_view(request):
+    context = {}
 
-	 context = {}
+    user = request.user
+    if user.is_authenticated:
+        return redirect("home")
 
-	 user = request.user
-	 if user.is_authenticated:
-	 	return redirect("home")
-
-	 if request.POST:
-	 	form = AccountAuthenticationForm(request.POST)
-	 	if form.is_valid():
-	 		email = request.POST['email']
-	 		password = request.POST['password']
-	 		user = authenticate(email=email, password=password)
-
-	 		if user:
-	 			login(request, user)
-	 			return redirect("home")
-
-	 else:
-	 	form = AccountAuthenticationForm()
-
-	 context['login_form'] = form
-	 return render(request, 'accounts/login.html', context)
+    if request.method == 'POST':
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, mark_safe(
+                    f'Welcome back, {request.user.first_name}! 🙂'))
+                return redirect("home")
+        else:
+            messages.error(request, "Whoops! Something doesn't seem "
+                                    "right. Please check your login "
+                                    "details.")
+    else:
+        form = AccountAuthenticationForm()
+    context['login_form'] = form
+    return render(request, 'accounts/login.html', context)
