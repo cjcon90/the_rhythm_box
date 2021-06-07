de7bf74872befe4552867284db0ce1b6c2446b2d
Ciaran Concannon <cjcon90@pm.me>
2021-06-07 15:09:51 +0100

    added add_to_cart functionality

--- rhythmbox/cart/urls.py
+++ rhythmbox/cart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import cart_detail
+from .views import cart_detail, add_to_cart
 
 urlpatterns = [
     path("cart/", cart_detail, name="cart"),
+    path("cart/add/<item_id>/", add_to_cart, name="add_to_cart")
 ]
