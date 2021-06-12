82719930dfcabcf598164f76b7bb269f2c95c076
Ciaran Concannon <cjcon90@pm.me>
2021-06-12 17:41:56 +0100

    added basic checkout functionality

--- rhythmbox/checkout/views.py
+++ rhythmbox/checkout/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render, redirect
 from .forms import OrderForm
 from django.conf import settings
+from cart.contexts import cart_contents
+import stripe
+
 
 def checkout(request):
     context = {}
-    cart = request.session.get('cart', {})
+
+    cart = request.session.get("cart", {})
     if not cart:
-        return redirect('cart')
-    context['order_form'] = OrderForm()
-    context['stripe_public_key'] = settings.STRIPE_PUBLIC_KEY
-    context['client_secret'] = 'shhhhh this is a secret'
-    return render(request, 'checkout/checkout.html', context)
+        return redirect("cart")
+    current_cart = cart_contents(request)
+    total = current_cart["grand_total"]
+    stripe_total = round(total * 100)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
+    context["order_form"] = OrderForm()
+
+    if not settings.STRIPE_PUBLIC_KEY:
+        messages.error(
+            request,
+            "Stripe public key is missing. Did you forget to set it in your environ?",
+        )
+
+    context["stripe_public_key"] = settings.STRIPE_PUBLIC_KEY
+    context["client_secret"] = intent.client_secret
+    return render(request, "checkout/checkout.html", context)
--- rhythmbox/static/scripts/stripeElements.js
+++ rhythmbox/static/scripts/stripeElements.js
@@ -1,23 +1,76 @@
-const stripe_public_key = document.getElementById('id_stripe_public_key').textContent;
-const stripe_client_secret = document.getElementById('id_client_secret').textContent;
-const cardElement = document.getElementById('card-element')
-console.log(cardElement)
+const stripePublicKey = document.getElementById(
+  "id_stripe_public_key"
+).textContent;
+const stripeClientSecret =
+  document.getElementById("id_client_secret").textContent;
+const cardElement = document.getElementById("card-element");
+const errorDiv = document.querySelector(".card-errors");
 
-console.log(stripe_public_key, stripe_client_secret)
-
-const stripe = Stripe(stripe_public_key);
+const stripe = Stripe(stripePublicKey);
 const elements = stripe.elements();
-const card = elements.create('card', {
-    classes: {
-        base: 'form__input regular',
+const card = elements.create("card", {
+  classes: {
+    base: "form__input regular",
+  },
+  style: {
+    base: {
+      fontSize: "1.5rem",
     },
-    style: {
-        base: {
-            fontSize: '1.5rem',
+    invalid: {
+      color: "#ad0000",
+    },
+  },
+});
+
+console.log(card);
+
+card.mount("#card-element");
+
+card.addEventListener("change", (e) => {
+  if (e.error) {
+    displayError(e, errorDiv);
+  } else {
+    errorDiv.innerHTML = "";
+  }
+});
+
+// Handle form submission
+
+const form = document.getElementById("payment-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card.update({ disabled: true });
+  stripe
+    .confirmCardPayment(stripeClientSecret, {
+      payment_method: {
+        card: card,
+        billing_details: {
+          name: "Mr Test Name",
+        },
+      },
+    })
+    .then((result) => {
+      if (result.error) {
+        displayError(result, errorDiv);
+        card.update({ disabled: false });
+      } else {
+        if (result.paymentIntent.status === "succeeded") {
+          form.submit();
         }
-    }
-  });
+      }
+    });
+});
 
-console.log(card)
-
-card.mount('#card-element');
+// display any errors in stripe process
+function displayError(error, div) {
+  const errorSpan = document.createElement("span");
+  errorSpan.classList.add("card-errors__error");
+  const i = document.createElement("i");
+  i.classList.add("fas", "fa-times", "card-errors__error--icon");
+  errorSpan.appendChild(i);
+  const textSpan = document.createElement("span");
+  textSpan.textContent = error.error.message;
+  div.appendChild(errorSpan);
+  div.appendChild(textSpan);
+}
