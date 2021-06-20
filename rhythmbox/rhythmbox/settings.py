42760de61bf99c2512bd6e94c212a39839a8ff7c
Ciaran Concannon <cjcon90@pm.me>
2021-06-20 18:41:49 +0100

    migrate to PostgreSQL database

--- rhythmbox/rhythmbox/settings.py
+++ rhythmbox/rhythmbox/settings.py
@@ -13,6 +13,7 @@ from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 import dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,11 +96,15 @@ WSGI_APPLICATION = "rhythmbox.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(os.environ['DATABASE_URL'])
 }
 
 # Custom message tags
