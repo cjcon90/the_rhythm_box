05a19a434ef816c92f1ec5536b972c01e730057e
Ciaran Concannon <cjcon90@pm.me>
2021-06-23 00:02:32 +0100

    add desktop dropdown navbar functionality

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -3,14 +3,30 @@
 const hamburger = document.querySelector(".nav__hamburger");
 const menu = document.querySelector(".nav-menu");
 const menuList = document.querySelectorAll(
-  "li.nav-menu__list--category, li.nav-menu__list--subcategory"
+  "li.nav-menu__list--category"
 );
 
 // Toggle hamburger menu and add animation to menu items
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
-  menu.classList.toggle("hidden");
+  menu.classList.toggle("nav-hidden");
   for (let i = 0; i < menuList.length; i++) {
     menuList[i].classList.toggle("nav-list-appear");
   }
 });
+
+// Hover dropdown functionality for desktop navbar
+for(let item of menuList) {
+  item.forEach(e => {
+    e.addEventListener('mouseover', () => {
+      makeVisible(e)
+      e.addEventListener('mouseover', () => {
+        makeVisible(e)
+      })
+    })
+  })
+}
+
+function makeVisible(element) {
+  element.setAttribute("style", "display: inline-block;");
+}
